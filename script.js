27af628ac7e43634f346c382cda6a8e75f2a17ef
Md. Zaid <73633706+zaidu7121@users.noreply.github.com>
2025-05-11 20:37:16 +0530

    scroll_top_btn_script.js

--- script.js
+++ script.js
@@ -120,3 +120,26 @@ document.querySelectorAll('.hover-item').forEach(item => {
     item.style.color = '#333'; // Reset color on hover out
   });
 });
+/// Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block"; // Show the button
+  } else {
+    mybutton.style.display = "none"; // Hide the button
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
